a0d2d100188b9baba2f5f528917ea41cc41cd1bf
Michael Borgwardt <michael@brazzy.de>
2020-12-29 20:14:43 +0000

    separate starting of game from logic so the latter can be tested

--- src/Galgenvogel.js
+++ src/Galgenvogel.js
@@ -107,16 +107,4 @@ class Galgenvogel {
 	}
 }
 
-var galgenvogel = new Galgenvogel();
-
-let config = {
-	create: galgenvogel.init.bind(galgenvogel),
-	update: galgenvogel.update.bind(galgenvogel),
-	onKeyPress: galgenvogel.onKeyPress.bind(galgenvogel),
-	onDotClicked: galgenvogel.onDotClicked.bind(galgenvogel),
-	frameRate: FRAME_RATE,
-	clearGrid: false
-};
-
-let game = new Game(config);
-game.run();
+export {Galgenvogel, FRAME_RATE}
---
+++ src/GalgenvogelStarter.js
@@ -0,0 +1,17 @@
+import { Galgenvogel, FRAME_RATE } from './Galgenvogel.js';
+
+var galgenvogel = new Galgenvogel();
+
+let config = {
+	create: galgenvogel.init.bind(galgenvogel),
+	update: galgenvogel.update.bind(galgenvogel),
+	onKeyPress: galgenvogel.onKeyPress.bind(galgenvogel),
+	onDotClicked: galgenvogel.onDotClicked.bind(galgenvogel),
+	frameRate: FRAME_RATE,
+	clearGrid: false
+};
+
+let game = new Game(config);
+game.run();
+
+export {Galgenvogel}
