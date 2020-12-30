4c65ac6b96dfa1b509c1155605b654d4cd47562b
Michael Borgwardt <michael@brazzy.de>
2020-12-30 23:01:49 +0100

    exclude coverage, refactor and add tests

--- test/Galgenvogel.test.js
+++ test/Galgenvogel.test.js
@@ -3,12 +3,14 @@ import { Color, Direction } from '../src/engine-types.js';
 import { Monster } from '../src/monster.js';
 import { jest } from '@jest/globals';
 
+const NO_WALLS = [[0,0,0], [0,0,0], [0,0,0]];
+
 describe('movement', () => {
 	
 	test('basic', () => {
 		const randomCoords = jest.fn();
 		randomCoords.mockImplementation( () => [1,1] );
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 0, randomCoords);
+		const gv = new Galgenvogel(() => NO_WALLS, 0, randomCoords);
 
 		gv.init(null);
 		expect(gv.player.x).toBe(1);
@@ -60,7 +62,7 @@ describe('movement', () => {
 	test('wraparound', () => {
 		const randomCoords = jest.fn();
 		randomCoords.mockImplementation( () => [0,0] );
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 0, randomCoords);
+		const gv = new Galgenvogel(() => NO_WALLS, 0, randomCoords);
 
 		gv.init(null);
 		expect(gv.player.x).toBe(0);
@@ -119,13 +121,36 @@ describe('movement', () => {
 		expect(gv.player.y).toBe(2);
 	});
 
+	test('monster follows', () => {
+		const randomCoords = jest.fn();
+		randomCoords
+			.mockImplementationOnce( () => [0,0] )
+			.mockImplementationOnce( () => [1,0] );
+		const finish = jest.fn();
+		const gv = new Galgenvogel(() => NO_WALLS, 1, randomCoords, finish, () => new Monster(Color.Orange, 2, 1, 10));
+
+		gv.init(null);
+		expect(gv.player.x).toBe(0);
+		expect(gv.player.y).toBe(0);
+		expect(gv.monsters.length).toBe(1);
+		expect(gv.monsters[0].x).toBe(1);
+		expect(gv.monsters[0].y).toBe(0);
+		
+		gv.onKeyPress(Direction.Down)
+		expect(gv.player.x).toBe(0);
+		expect(gv.player.y).toBe(1);
+		expect(gv.monsters.length).toBe(1);
+		expect(gv.monsters[0].x).toBe(1);
+		expect(gv.monsters[0].y).toBe(1);
+	});
+
 	test('exchange blows', () => {
 		const randomCoords = jest.fn();
 		randomCoords
 			.mockImplementationOnce( () => [1,1] )
 			.mockImplementationOnce( () => [2,1] );
 		const finish = jest.fn();
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 1, randomCoords, finish, () => new Monster(Color.Orange, 2, 1, 10));
+		const gv = new Galgenvogel(() => NO_WALLS, 1, randomCoords, finish, () => new Monster(Color.Orange, 2, 1, 10));
 
 		gv.init(null);
 		expect(gv.player.x).toBe(1);
@@ -151,7 +176,7 @@ describe('movement', () => {
 			.mockImplementationOnce( () => [1,1] )
 			.mockImplementationOnce( () => [2,1] );
 		const finish = jest.fn();
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 1, randomCoords, finish, () => new Monster(Color.Orange, 1, 1, 10));
+		const gv = new Galgenvogel(() => NO_WALLS, 1, randomCoords, finish, () => new Monster(Color.Orange, 1, 1, 10));
 
 		gv.init(null);
 		expect(gv.player.health).toBe(3);
@@ -170,7 +195,7 @@ describe('movement', () => {
 			.mockImplementationOnce( () => [1,1] )
 			.mockImplementationOnce( () => [2,1] );
 		const finish = jest.fn();
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 1, randomCoords, finish, () => new Monster(Color.Orange, 5, 3, 10));
+		const gv = new Galgenvogel(() => NO_WALLS, 1, randomCoords, finish, () => new Monster(Color.Orange, 5, 3, 10));
 
 		gv.init(null);
 		expect(gv.player.health).toBe(3);
@@ -190,7 +215,7 @@ describe('movement', () => {
 			.mockImplementationOnce( () => [1,1] )
 			.mockImplementationOnce( () => [2,1] );
 		const finish = jest.fn();
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 1, randomCoords, finish, () => new Monster(Color.Orange, 1, 1, 10));
+		const gv = new Galgenvogel(() => NO_WALLS, 1, randomCoords, finish, () => new Monster(Color.Orange, 1, 1, 10));
 
 		gv.init(null);
 		expect(gv.player.health).toBe(3);
@@ -210,7 +235,7 @@ describe('movement', () => {
 			.mockImplementationOnce( () => [1,1] )
 			.mockImplementationOnce( () => [2,1] );
 		const finish = jest.fn();
-		const gv = new Galgenvogel(() => [[0,0,0], [0,0,0], [0,0,0]], 0, randomCoords);
+		const gv = new Galgenvogel(() => NO_WALLS, 0, randomCoords);
 
 		gv.init(null);
 		expect(gv.player.x).toBe(1);
